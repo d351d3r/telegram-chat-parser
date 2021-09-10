73c350ea7469cbacc1c18b7c3aba626c41a4e417
Anatolii Cherednichenko <greatestcombinator@gmail.com>
2021-09-10 22:21:31 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SaveMyTG',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Anatolii Cherednichenko',
+    author_email='greatestcombinator@gmail.com',
+    description=''
+)
